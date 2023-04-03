bc138d6a3b4cbf48b626280ce4364b55e9d93244
Alex Kamenka <kamenka993@gmail.com>
2023-04-03 04:39:33 +0300

    -imports

--- src/telran/chat/client/controller/ChatClientAppl.java
+++ src/telran/chat/client/controller/ChatClientAppl.java
@@ -1,10 +1,8 @@
 package telran.chat.client.controller;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.OutputStream;
 import java.net.Socket;
 
 import javax.swing.JOptionPane;
